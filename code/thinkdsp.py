9c1d85b4caa01394ccb54c2fd53a0a29e55d76d8
1iuke <1264746554@qq.com>
2024-10-30 18:00:39 +0800

    read

--- code/thinkdsp.py
+++ code/thinkdsp.py
@@ -1567,7 +1567,23 @@ class ExpoChirp(Chirp):
         ys = self.amp * np.cos(phases)
         return ys
 
+class SawtoothChirp(Chirp):
+    """Represents a sawtooth signal with varying frequency."""
 
+    def evaluate(self, ts):
+        """Helper function that evaluates the signal.
+
+        ts: float array of times
+        """
+        freqs = np.linspace(self.start, self.end, len(ts))
+        dts = np.diff(ts, prepend=0)
+        dphis = PI2 * freqs * dts
+        phases = np.cumsum(dphis)
+        cycles = phases / PI2
+        frac, _ = np.modf(cycles)
+        ys =  normalize(unbias(frac), self.amp)
+        return ys
+        
 class SilentSignal(Signal):
     """Represents silence."""
 
